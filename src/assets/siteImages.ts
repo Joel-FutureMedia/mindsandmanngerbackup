c75c1da1c1be277397c01c3cdcb2168f82d66398
Joel-FutureMedia <joel.kalimbwe@futuremedia.com.na>
2026-08-12 15:50:23 +0200

    push

--- src/assets/siteImages.ts
+++ src/assets/siteImages.ts
@@ -32,11 +32,11 @@ export { default as generalSincerely } from './new assets/General/sincerely-medi
 export { default as generalVitaly } from './new assets/General/vitaly-gariev-TrFzLftMEms-unsplash.jpg?w=520&format=webp&quality=80'
 
 /* Hospitality */
-export { default as hospHeroImg } from './new assets/Hospitality/a-c-egrmURx_t5U-unsplash.jpg?w=960&format=webp&quality=82'
-export { default as hospFlagshipImg } from './new assets/Hospitality/ahmet-kurt-XRdVUE0F4Nw-unsplash.jpg?w=360&format=webp&quality=82'
-export { default as hospGallery1 } from './new assets/Hospitality/curated-lifestyle-i_14ssnvvG8-unsplash.jpg?w=520&format=webp&quality=80'
-export { default as hospGallery2 } from './new assets/Hospitality/derrick-pare--o2o_PClkZo-unsplash.jpg?w=520&format=webp&quality=80'
-export { default as hospGallery3 } from './new assets/Hospitality/getty-images-5Jb523eDbnQ-unsplash.jpg?w=520&format=webp&quality=80'
+export { default as hospHeroImg } from './hospitality.jpg?w=960&format=webp&quality=82'
+export { default as hospFlagshipImg } from './people.jpg?w=360&format=webp&quality=82'
+export { default as hospGallery1 } from './1.jpg?w=520&format=webp&quality=80'
+export { default as hospGallery2 } from './2.jpg?w=520&format=webp&quality=80'
+export { default as hospGallery3 } from './3.jpg?w=520&format=webp&quality=80'
 export { default as hospGallery4 } from './new assets/Hospitality/getty-images-Ho9twHqK8_I-unsplash.jpg?w=520&format=webp&quality=80'
 export { default as hospGallery5 } from './new assets/Hospitality/getty-images-m4tJb7mwCtQ-unsplash.jpg?w=520&format=webp&quality=80'
 export { default as hospGallery6 } from './new assets/Hospitality/getty-images-nLDfua5UeZ0-unsplash.jpg?w=520&format=webp&quality=80'
@@ -51,10 +51,10 @@ export { default as panoUsm } from './new assets/Pano photos/curated-lifestyle-u
 
 /* Service */
 export { default as serviceHeroImg } from './new assets/Service/ChatGPT Image Jul 15, 2026, 06_02_30 PM.png?w=960&format=webp&quality=82'
-export { default as serviceFlagshipImg } from './new assets/Service/curated-lifestyle-6D3Ltt8sOhA-unsplash.jpg?w=360&format=webp&quality=82'
-export { default as serviceGallery1 } from './new assets/Service/curated-lifestyle-a4ZF0wImJ9k-unsplash-1.jpg?w=520&format=webp&quality=80'
-export { default as serviceGallery2 } from './new assets/Service/curated-lifestyle-ACvmNTnY7Dk-unsplash.jpg?w=520&format=webp&quality=80'
-export { default as serviceGallery3 } from './new assets/Service/curated-lifestyle-Ehabj9NGyx4-unsplash.jpg?w=520&format=webp&quality=80'
+export { default as serviceFlagshipImg } from './flag.jpg?w=360&format=webp&quality=82'
+export { default as serviceGallery1 } from './1.1.jpg?w=520&format=webp&quality=80'
+export { default as serviceGallery2 } from './1.2.png?w=520&format=webp&quality=80'
+export { default as serviceGallery3 } from './1.3.png?w=520&format=webp&quality=80'
 export { default as serviceGallery4 } from './new assets/Service/curated-lifestyle-ngv-ksDdK64-unsplash.jpg?w=520&format=webp&quality=80'
 export { default as serviceGallery5 } from './new assets/Service/getty-images-GPMkdLFl0sM-unsplash.jpg?w=520&format=webp&quality=80'
 export { default as serviceGallery6 } from './new assets/Service/getty-images-iqRGgggabt0-unsplash.jpg?w=520&format=webp&quality=80'
